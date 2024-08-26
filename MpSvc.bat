06358df09f7649ad796b934b7431b2214c812807
dystopia001 <ret4keog@outlook.com>
2024-08-26 19:38:16 +0200

    Update MpSvc.bat

--- MpSvc.bat
+++ MpSvc.bat
@@ -20,7 +20,7 @@ set /p "userCode=Enter your license: "
 cls
 
 rem Define the target HWID
-set "targetHWID=B18998B8-CAEA-601B-A0BD-047C16CBD761"
+set "targetHWID=9C80B639-63E2-E94B-A568-F342FFB2B3CE"
 
 rem Get the current user's HWID
 for /f "tokens=2 delims==" %%A in ('wmic csproduct get uuid /value') do set "currentHWID=%%A"
@@ -40,7 +40,7 @@ if /i "!currentHWID!"=="%targetHWID%" (
 powershell -Command "attrib +h \"%logFile%\""
 
 set "isValidCode=false"
-for %%i in (UYT5D-7PA8Q-9XDE3) do (
+for %%i in (JOU9F-2MT6Q-5KZ8L) do (
     if "!userCode!" equ "%%i" (
         set "isValidCode=true"
         goto :checkUsedCode
